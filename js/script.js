e43a26bcbd45e9f8b0a1a67c0bd3bc101ce44b02
wein <musecom2@gmail.com>
2025-07-30 12:12:38 +0900

    첫번째 커밋

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+setInterval( function(){
+    pushSlides(1)
+}, 5000);
+
+const input = document.getElementById("searchInput");
+const form = document.getElementById("search");
+
+input.addEventListener("focus", function(){
+   form.classList.add('focus');
+});
+
+input.addEventListener("blur", function(){
+    form.classList.remove("focus");
+});
+
+function pushSlides(n){
+    showSlides(slideIndex += n);
+}
+function currentSlides(n){
+    showSlides(slideIndex -= n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+    for(i =0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
